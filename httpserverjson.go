0f65d5d10bf0c4d55ab6a54a8a53d7c5d7839208
dandeon <104778232+dandde@users.noreply.github.com>
2022-05-10 15:14:54 +0800

    Update httpserverjson.go

--- httpserverjson.go
+++ httpserverjson.go
@@ -1,10 +1,20 @@
 package main
+
 import "github.com/gin-gonic/gin"
+
 func IndexHandler(c *gin.Context){
-  c.JSON(200, gin.H{"message": "hello world",})}
+  //c.JSON(200, gin.H{"message": "hello world",})
+  name := c.Params.ByName("name")
+  c.JSON(200, gin.H{
+    "message": "hello " + name,
+  })
+}
 
 func main() {
-    router := gin.Default()
-    router.GET("/", IndexHandler)
-    router.Run()
+    //router := gin.Default()
+    //router.GET("/", IndexHandler)
+    //router.Run()
+  router := gin.Default()
+  router.GET("/:name", IndexHandler)
+  router.Run()
 }
